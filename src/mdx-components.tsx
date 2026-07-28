2cd873f9828b8fb7d3b037b4ddba0f60b35638e1
Michael Romashov <mromashov@icloud.com>
2026-07-28 01:30:41 -0400

    Docs on configuring liveries

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,3 +1,4 @@
+import { CodeBlock, Pre } from "fumadocs-ui/components/codeblock";
 import { ImageZoom } from "fumadocs-ui/components/image-zoom";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
@@ -7,6 +8,11 @@ export function getMDXComponents(components?: MDXComponents): MDXComponents {
     return {
         ...defaultMdxComponents,
         img: (props) => <ImageZoom {...(props as any)} />,
+        pre: ({ ref: _ref, ...props }) => (
+            <CodeBlock {...props}>
+                <Pre>{props.children}</Pre>
+            </CodeBlock>
+        ),
         LinkButton,
         ...components,
     };
